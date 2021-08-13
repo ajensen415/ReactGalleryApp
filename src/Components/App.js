c2da0c5445e5d12f55a44be679bbce84e91b515c
ajensen415 <ajensen415@yahoo.com>
2021-08-13 14:25:34 -0400

    //

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,3 +1,53 @@
+// import React, { Component } from 'react';
+// import {
+//   BrowserRouter as Router,
+//   Route,
+//   Switch
+// } from 'react-router-dom';
+
+// import apiKey from '../config';
+
+// import Nav from './Nav';
+// import NotFound from './NotFound';
+// import Search from './Search';
+// //import PhotoContainer from './PhotoContainer';
+
+// class App extends Component {
+// //TODO - pull in data from API 
+// //const props = 
+
+//   //set state & mount 
+//   /*setState({
+//     photos: [],
+//     page: 1,
+//     perPage: 24,
+//     isLoading: false*/
+
+//   //function search images tags
+
+//   //render & routes
+//   render() {
+//     return (
+//       <div>
+//         <Search />
+//         <Router>
+//           <Route path='/sloths'>
+//             This will be Sloths! 
+//           </Route>
+//           <Route path='/newyork'>
+//             This will be New York! 
+//           </Route>
+//           <Route path='/mountains'>
+//             This will be Mountains! 
+//           </Route>
+//         </Router>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
+
 import React, { Component } from 'react';
 import {
   BrowserRouter as Router,
@@ -12,38 +62,49 @@ import NotFound from './NotFound';
 import Search from './Search';
 //import PhotoContainer from './PhotoContainer';
 
-class App extends Component {
-//TODO - pull in data from API 
-//const props = 
+export default class App extends Component {
+  
+  constructor() {
+    super();
+    this.state = {
+      photos: []
+    };
+  } 
 
-  //set state & mount 
-  /*setState({
-    photos: [],
-    page: 1,
-    perPage: 24,
-    isLoading: false*/
+  componentDidMount() {
 
-  //function search images tags
-
-  //render & routes
-  render() {
+  }
+  
+  performSearch = (query) => {
+    axios.get(`http://api.giphy.com/v1/gifs/search?q=${query}&limit=24&api_key=dc6zaTOxFJmzC`)
+      .then(response => {
+        this.setState({
+          gifs: response.data.data
+        });
+      })
+      .catch(error => {
+        console.log('Error fetching and parsing data', error);
+      });    
+  }
+  
+  render() { 
+    console.log(this.state.gifs);
     return (
       <div>
-        <Search />
-        <Router>
-          <Route path='/sloths'>
-            This will be Sloths! 
-          </Route>
-          <Route path='/newyork'>
-            This will be New York! 
-          </Route>
-          <Route path='/mountains'>
-            This will be Mountains! 
-          </Route>
-        </Router>
+        <div className="main-header">
+          <div className="inner">
+            <h1 className="main-title">GifSearch</h1>
+            <SearchForm onSearch={this.performSearch} />      
+          </div>   
+        </div>    
+        <div className="main-content">
+          <GifList data={this.state.gifs} />
+        </div>
       </div>
     );
   }
 }
 
-export default App;
+
+
+
--- src/Components/PhotoContainer.js
+++ src/Components/PhotoContainer.js
@@ -1,28 +1,54 @@
-import React from 'react';
-import { withRouter } from 'react-router-dom';
+// import React from 'react';
+// import { withRouter } from 'react-router-dom';
 
+// import NotFound from './NotFound';
+// import Photo from './Photo';
+
+// //function to iterate through the returned images & provide them with a unique key(24 images)
+// const PhotoContainer = (props) => {
+
+//     let images = props.data.map ( photo =>
+//         <Photo 
+//             url={`https://live.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`}
+//             key={photo.id}
+//         />
+//     );
+
+//     //return images into container, adding to ul
+//     return (
+//         <div className="photo-contasiner">
+//             <h2>Results: {props.title}</h2>
+//                 <ul>
+//                     {photos}
+//                 </ul>
+//         </div>
+//     );
+// }
+
+// export default PhotoContainer;
+
+import React from 'react';
 import NotFound from './NotFound';
 import Photo from './Photo';
 
-//function to iterate through the returned images & provide them with a unique key(24 images)
-const PhotoContainer = (props) => {
-
-    let images = props.data.map ( photo =>
-        <Photo 
-            url={`https://live.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`}
-            key={photo.id}
-        />
-    );
-
-    //return images into container, adding to ul
-    return (
-        <div className="photo-contasiner">
-            <h2>Results: {props.title}</h2>
-                <ul>
-                    {photos}
-                </ul>
-        </div>
-    );
-}
+const Photo = props => { 
+  
+  const results = props.data;
+  let images = results.map(image =>
+    <Photo 
+        url={`https://live.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`}
+        key={photo.id} 
+    />
+  );
+  
+  return (
+            <div className="photo-contasiner">
+                <h2>Results: {props.title}</h2>
+                    <ul>
+                        {photos}
+                    </ul>
+            </div>
+        );
+    }
 
 export default PhotoContainer;
